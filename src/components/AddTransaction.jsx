39756d7282fe405698256af97a3d4baa072aa212
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-25 22:16:42 +0530

    added vertical space between Balance & AddTransaction

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -2,7 +2,8 @@
 function AddTransaction() {
     return (
         <div>
-            <input className="w-96 rounded-3xl px-5 py-1 bg-neutral-700 text-white text-xl font-bold outline-none" type="text" placeholder="Add transactions ..."/>
+            <input className="w-96 rounded-xl px-3 py-1 bg-neutral-700 text-white text-xl font-bold outline-none" type="text" placeholder="Add transactions ..."/>
+            <button type="button"><i></i></button>
         </div>
     );
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,9 +3,9 @@ import AddTransaction from "./AddTransaction";
 
 function Dashboard() {
     return (
-        <div className="h-full p-4 space-y-96">
+        <div className="h-screen p-4 space-y-96">
             <div className="rounded-lg px-2 py-1 bg-neutral-700 text-white text-3xl font-bold shadow-lg"><Balance /></div>
-            <div className="flex justify-center outline-none"><AddTransaction /></div>
+            <div className="flex justify-center outline-none px-1 py-1"><AddTransaction /></div>
         </div>
     );
 }
