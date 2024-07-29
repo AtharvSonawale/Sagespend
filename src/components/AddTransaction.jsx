ea2d1248b8125f52dac287b331eb90e86f7c5baf
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-29 23:05:13 +0530

    added colors to Balance and AddTransaction component

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -2,7 +2,7 @@
 function AddTransaction() {
     return (
         <div>
-            <input className="w-96 rounded-xl px-3 py-1 bg-neutral-700 text-white text-xl font-bold outline-none" type="text" placeholder="Add transactions ..."/>
+            <input className="w-96 rounded-xl px-3 py-1 bg-slate-600 text-white text-xl font-extralight outline-none" type="text" placeholder="Add transactions here..."/>
             <button type="button"><i></i></button>
         </div>
     );
